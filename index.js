e7422d049ceb11d4d316cb8de7147ef0a47af79a
AXEL JOHN TEJUCO (IM-MIS-ISD-OOCLL/MNL) <TEJUCAX@OOCL.COM>
2022-08-18 14:25:33 +0800

    initial push

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
+name = name.toUpperCase();
+console.log(name)
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+splittedSentence = sentence.split(" ");
+var upperSentence = '';
+for(var i = 0; i < splittedSentence.length; i++){
+    splittedSentence[i] = splittedSentence[i].charAt(0).toUpperCase() + splittedSentence[i].slice(1);
+    upperSentence = upperSentence + ' ' + splittedSentence[i];
+}
+console.log(upperSentence);
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
+var newMoney = money.split("￥");
+console.log(newMoney[1].trim());
